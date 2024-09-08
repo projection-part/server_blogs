b5522f84849f9880c3bfab45c03f2441051dd895
umesh <umeshbltr9@gmail.com>
2024-09-08 13:35:44 +0530

    cors policy updated

--- middleware/cors.middleware.js
+++ middleware/cors.middleware.js
@@ -1,3 +1,9 @@
 const cors = require("cors");
 
-module.exports = cors();
+const corsOptions = {
+    origin: '*', // Replace with your client's domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // if your client needs to send cookies with the requests
+  };
+
+module.exports = cors(corsOptions);
